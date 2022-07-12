7c3421f226fed44a0aa79a42c9f075bd2e462043
Sagi Elhayani <sagielhayani@gmail.com>
2022-07-12 21:43:17 +0300

    Fixed Code

--- spring-boot-package-war/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-package-war/src/main/java/com/neo/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.neo.controller;
+package com.neo.spring.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloWorldController {
 	
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World xx";
+        return "Hello World";
     }
 }
--- spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTests.java
+++ spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTests.java
@@ -1,4 +1,4 @@
-package com.neo.controller;
+package com.neo.spring.boot.controller;
 
 import org.junit.Before;
 import org.junit.Test;
